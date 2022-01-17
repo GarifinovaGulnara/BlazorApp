af60dc68e8afe15e98e6d9b7864c4c8d6a2710ea
Гульнара Гарифинова <gulnaragarifinova@yandex.ru>
2022-01-17 08:41:21 +0300

    create classes

---
+++ BlazorApp/Data/Auto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Data
+{
+    public class Auto
+    {
+        public string Name { get; set; }
+        public string Release { get; set; }
+        public int Price { get; set; }
+
+        public Auto(string name, string releaes, int price)
+        {
+            Name = name;
+            Release = releaes;
+            Price = price;
+        }
+
+        public static List<Auto> GetStudentInfo()
+        {
+            List<Auto> auto = new List<Auto>();
+            auto.Add(new Auto("M", "25.05.2009", 4500000));
+            auto.Add(new Auto("L", "03.04.2005", 3500000));
+            auto.Add(new Auto("P", "05.11.2006", 5000000));
+            auto.Add(new Auto("T", "01.05.2001", 3350000));
+            auto.Add(new Auto("Maz", "10.10.2003", 3750000));
+            return auto;
+        }
+    }
+}
---
+++ BlazorApp/Data/Games.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Data
+{
+    public class Games
+    {
+        public string Name { get; set; }
+        public string Release { get; set; }
+        public double Rating { get; set; }
+
+        public Games(string name, string release, double rating)
+        {
+            Name = name;
+            Release = release;
+            Rating = rating;
+        }
+
+        public static List<Games> GetStudentInfo()
+        {
+            List<Games> game = new List<Games>();
+            game.Add(new Games("GI", "21.05.2003", 4.95));
+            game.Add(new Games("T", "04.04.2004", 4.85));
+            game.Add(new Games("SR", "7.06.2019", 4.5));
+            game.Add(new Games("F", "15.10.2015", 3.4));
+            game.Add(new Games("As", "08.09.2010", 4));
+            return game;
+        }
+    }
+}
